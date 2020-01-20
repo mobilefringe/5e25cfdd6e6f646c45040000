803ad9cfcfc301d4cb8aa08a569826c786a7770f
mithun@mobilefringe.com @ CodeCloud <mithun@mobilefringe.com @ CodeCloud>
2020-01-20 11:41:45 -0500

    Costa Mesa 3.0 1.0  /center_information.vue

--- center_information.vue
+++ center_information.vue
@@ -45,7 +45,7 @@
 </template>
 
 <script>
-	define(["Vue", "vuex"], function(Vue, Vuex) {
+	define(["Vue", "vuex", "json!site.json"], function(Vue, Vuex, Site) {
 		return Vue.component("center-info-component", {
             template: template, // the variable template will be injected
             data: function () {
@@ -94,7 +94,7 @@
                 loadData: async function () {
                     this.property.mm_host = this.property.mm_host.replace("http:", "");
                     try {
-                        let results = await Promise.all([this.$store.dispatch("getData", "repos"), this.$store.dispatch('LOAD_PAGE_DATA', { url: this.property.mm_host + "/pages/costamesa-center-information.json" })]);
+                        let results = await Promise.all([this.$store.dispatch("getData", "repos"), this.$store.dispatch('LOAD_PAGE_DATA', { url: this.property.mm_host + "/pages/"+ Site.subdomain + "-center-information.json" })]);
                         return results;
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
