8db88ff4a3820725a97b99bd1755d8b9eaa8078f
mithun@mobilefringe.com @ CodeCloud <mithun@mobilefringe.com @ CodeCloud>
2020-01-20 12:22:09 -0500

    Costa Mesa 3.0 1.0  /main.js

--- main.js
+++ main.js
@@ -189,7 +189,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
         methods: {
             loadData: async function() {
                 try{
-                    await this.$store.dispatch('initializeApi', { site: "costamesa", version: "v4" });
+                    await this.$store.dispatch('initializeApi', { site: site.subdomain, version: "v4" });
                     await Promise.all([this.$store.dispatch("getData", "property")]);
                     this.property.mm_host = this.property.mm_host.replace("http:", "");
                     let results = await Promise.all([
