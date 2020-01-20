1d174e40c9131d0e5d6d7e319b53133bafef3728
mithun@mobilefringe.com @ CodeCloud <mithun@mobilefringe.com @ CodeCloud>
2020-01-20 13:21:22 -0500

    Costa Mesa 3.0 1.0  /promotion_details.vue

---
+++ promotion_details.vue
@@ -0,0 +1,150 @@
+<template>
+    <div> <!-- Without an outer container div this component template will not render -->
+        <loading-spinner v-if="!dataLoaded"></loading-spinner>
+        <transition name="fade">
+            <div v-if="dataLoaded" v-cloak>
+                <div class="inside_page_header" v-if="pageBanner" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
+                    <div class="main_container position_relative">
+                        <h1>Events & Promotions</h1>
+                    </div>
+                </div>
+                <div class="main_container">
+                    <div class="row hidden-xs">
+                        <div class="col-md-12">
+                            <breadcrumb></breadcrumb>
+                        </div>
+                    </div>
+                    <div v-if="currentPromo">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <p v-if="currentPromo.promotionable_type == 'Property'" class="event_store_name">{{ property.name }}</p>
+                                <p v-else class="event_store_name">{{ currentPromo.store.name }}</p>
+                                <h2 class="event_name">{{ currentPromo.name }}</h2>
+                                <p class="event_dates" v-if="isMultiDay(currentPromo)">{{ currentPromo.start_date | moment("MMMM D", timezone)}} - {{ currentPromo.end_date | moment("MMMM D", timezone)}}</p>
+                                <p class="event_dates" v-else>{{ currentPromo.start_date | moment("MMMM D", timezone)}}</p>
+                                <div class="event_desc event_details" v-html="currentPromo.rich_description"></div>
+                            </div>
+                            <div class="col-md-4">
+                                <a :href="currentPromo.image_url" :data-lightbox="currentPromo.name">
+                                    <img v-lazy="currentPromo.image_url" :alt="'Promotion: ' + currentPromo.name" class="margin_20 img_max"/>    
+                                </a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="row margin_30">
+                                    <div class="col-md-12">
+                                        <router-link to="/events-and-promotions">
+                    		                <div class="animated_btn pull-left">Back to Events & Promotions</div>    
+                    		            </router-link>    
+                                    </div>
+                                </div>
+                                <social-sharing v-if="currentPromo" :url="shareURL(currentPromo.slug)" :title="currentPromo.name" :description="currentPromo.description" :quote="truncate(currentPromo.description)" :media="currentPromo.image_url" inline-template>
+                                    <div class="social_share margin_60">
+                                        <network network="facebook">
+                                            <i class="fab fa-facebook"></i>
+                                        </network>
+                                        <network network="twitter">
+                                            <i class="fab fa-twitter"></i>
+                                        </network>
+                                        <network network="email">
+                                            <i class="fas fa-envelope"></i>
+                                        </network>
+                                    </div>
+                                </social-sharing>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "lightbox", "vue-lazy-load",  "vue-social-sharing", "json!site.json"], function(Vue, Vuex, moment, tz, VueMoment, Lightbox, VueLazyload, SocialSharing, site) {
+        Vue.use(VueLazyload);
+        Vue.component('social-sharing', SocialSharing);
+        return Vue.component("promo-details-component", {
+            template: template, // the variable template will be injected,
+            props: ['id'],
+            data: function() {
+                return {
+                    dataLoaded: false,
+                    pageBanner: null,
+                    currentPromo: null,
+                    siteInfo: site
+                }
+            },
+            created() {
+                this.$store.dispatch("getData", "repos").then(response => {
+			        var temp_repo = this.findRepoByName('Events Banner').images;
+                    if(temp_repo != null) {
+                        this.pageBanner = temp_repo[0];
+                    } else {
+                        this.pageBanner = {
+                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5de7dca36e6f6435b2020000/image/jpeg/1529532304000/insidebanner2.jpg"
+                        }
+                    }
+			    }, error => {
+					console.error("Could not retrieve data from server. Please check internet connection and try again.");
+				});
+				
+				this.$store.dispatch("getData", "promotions").then(response => {
+					this.currentPromo = this.findPromoBySlug(this.id);
+					if (this.currentPromo === null || this.currentPromo === undefined) {
+						this.$router.replace({ path: '/events-and-promotions' });
+					}
+					this.$breadcrumbs[0].path = "/events-and-promotions"
+					this.$breadcrumbs[1].meta.breadcrumb = this.currentPromo.name
+					this.dataLoaded = true;
+				}, error => {
+					console.error("Could not retrieve data from server. Please check internet connection and try again.");
+				});
+			},
+			watch: {
+                currentPromo : function (){
+                    if(this.currentPromo != null) {
+                        if (this.currentPromo.promotionable_type === "Store"){
+                            if  (_.includes(this.currentPromo.promo_image_url_abs, 'missing')) {
+                                this.currentPromo.image_url = this.currentPromo.store.store_front_url_abs; 
+                            }
+                        } else {
+                            if  (_.includes(this.currentPromo.promo_image_url_abs, 'missing')) {
+                                this.currentPromo.image_url = "//codecloud.cdn.speedyrails.net/sites/5de7dca36e6f6435b2020000/image/png/1529532181000/promoplaceholder2@2x.png";    
+                            }
+                        }
+                    }
+                }
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'timezone',
+                    'findRepoByName',
+                    'findPromoBySlug'
+                ])
+            },
+            methods: {
+				isMultiDay(currentPromo) {
+					var timezone = this.timezone
+					var start_date = moment(currentPromo.start_date).tz(timezone).format("MM-DD-YYYY")
+					var end_date = moment(currentPromo.end_date).tz(timezone).format("MM-DD-YYYY")
+					if (start_date === end_date) {
+						return false
+					} else {
+						return true
+					}
+				},
+				truncate(val_body) {
+                    var truncate = _.truncate(val_body, { 'length': 99, 'separator': ' ' });
+                    return truncate;
+                },
+				shareURL(slug) {
+                    var share_url = window.location.href
+                    return share_url
+                }
+			}
+        });
+    });
+</script>
