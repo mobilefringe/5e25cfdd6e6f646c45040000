6d9fcfe07b5df3dc226b2d9eb64bcf8f386e5d8b
mithun@mobilefringe.com @ CodeCloud <mithun@mobilefringe.com @ CodeCloud>
2020-01-21 13:11:50 -0500

    Costa Mesa 3.0 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -31,7 +31,7 @@
     						<ul>
     						    <li class="menu_item" v-for="item in menu_items" :id="item.id">
     						        <router-link v-if="item.sub_menu == undefined" :to="item.href">{{ item.name }}</router-link>
-    						        <span  v-if="item.sub_menu != undefined">{{ item.name }}</span>
+    						        <span tabindex=0 v-if="item.sub_menu != undefined">{{ item.name }}</span>
     						        <ul v-if="item.sub_menu" class="subdropdown">
     						            <li v-for="sub_menu in item.sub_menu" class="dropdown_item">
     						                <router-link :to="sub_menu.href">
