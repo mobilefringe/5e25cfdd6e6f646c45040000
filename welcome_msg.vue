bc9d4efb836326afa2ff6c017698bbbf0ae17503
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-05-06 11:48:49 -0400

    Costa Mesa 3.0 1.0  /welcome_msg.vue

---
+++ welcome_msg.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-if="property" class="main_container">
+        <div class="message_content">
+            <h1 class="center" v-if="property.welcome_message">{{ property.welcome_message }}</h1>
+            <p class="center" v-if="property.description">{{ property.description }}</p> 
+        </div>
+    </div>
+</template>
+
+<script>
+    define(["Vue", "vuex"], function(Vue, Vuex){
+        return Vue.component("messages-component", {
+            template: template, // the variable template will be injected
+            computed: {
+                ...Vuex.mapGetters([
+                    'property'
+                ])
+            },
+            created(){
+                console.log(this.property)
+            }
+        })
+    });
+</script>
