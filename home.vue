4a29ced77c1e7ae36ba4dd1af46405ce36c085f7
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-05-14 15:45:16 -0400

    Costa Mesa 3.0 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -332,18 +332,6 @@
                         return true
                     }
                 }
-            },
-            metaInfo () {
-                return {
-                    title: this.meta.meta_title,
-                    meta: [
-                        { name: 'description', vmid: 'description', content: this.meta.meta_description },
-                        { name: 'keywords',  vmid: 'keywords', content: this.meta.meta_keywords },
-                        { property: 'og:title', vmid: 'og:title', content: this.meta.meta_title },
-                        { property: 'og:description', vmid: 'og:description', content: this.meta.meta_description },
-                        { property: 'og:image', vmid: 'og:image', content: this.meta.meta_image }
-                    ]
-                }
             }
         });
     })
