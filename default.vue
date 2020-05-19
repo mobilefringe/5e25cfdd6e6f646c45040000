5fd97539b4a7a49a6fa95a7a772827994e74cc74
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-05-19 16:32:06 -0400

    Costa Mesa 3.0 1.0  /default.vue

--- default.vue
+++ default.vue
@@ -3,27 +3,28 @@
 </template>
 
 <script>
-    define(["Vue"], function(Vue) {
+    define(["Vue", "vue-meta"], function(Vue, Meta) {
+        Vue.use(Meta);
         return Vue.component("default-component", {
             template: template, // the variable template will be injected
-            head: {
-                // To use "this" in the component, it is necessary to return the object through a function
-                title: function () {
-                  return {
-                    inner: this.meta.meta_title,
-                    separator: ' ', // Leave empty separator
-                    complement: ' ' // Leave empty complement
-                  }
-                },
-                meta: function () {
-                  return [
-                     { name: 'description', id: 'description', content: this.meta.meta_description },
-                     { name: 'keywords',  id: 'keywords', content: this.meta.meta_keywords },
-                     { property: 'og:title', id: 'og:title', content: this.meta.meta_title },
-                     { property: 'og:description', id: 'og:description', content: this.meta.meta_description }
-                  ]
-                }
-            },
+            // head: {
+            //     // To use "this" in the component, it is necessary to return the object through a function
+            //     title: function () {
+            //       return {
+            //         inner: this.meta.meta_title,
+            //         separator: ' ', // Leave empty separator
+            //         complement: ' ' // Leave empty complement
+            //       }
+            //     },
+            //     meta: function () {
+            //       return [
+            //          { name: 'description', id: 'description', content: this.meta.meta_description },
+            //          { name: 'keywords',  id: 'keywords', content: this.meta.meta_keywords },
+            //          { property: 'og:title', id: 'og:title', content: this.meta.meta_title },
+            //          { property: 'og:description', id: 'og:description', content: this.meta.meta_description }
+            //       ]
+            //     }
+            // },
             data: function() {
                 return {
                     meta: {
